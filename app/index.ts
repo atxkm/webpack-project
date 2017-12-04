6b85e720539724020edfbb87cb24726bfac65e32
tangxiaokui <tangxiaokui@vwvo.com>
2017-12-04 09:32:22 +0800

    暂时Over

--- app/index.ts
+++ app/index.ts
@@ -1,2 +1,6 @@
 import './css/common.scss';
 import './index.scss';
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  window.location.href = "http://www.moni0.com/download";
+}
